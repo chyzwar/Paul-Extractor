648741c4f55f11d6d1f344f002ffbf93ee397857
unknown <marcin.kopacz@AuroreCarric-PC.caliberi.local>
2014-12-17 12:06:33 +0000

    windows

--- windows/PaulTheExtracter.py
+++ windows/PaulTheExtracter.py
@@ -8,13 +8,12 @@ import shutil
 Tk().withdraw()
 
 zip_file = askopenfilename(
-    initialdir="/home/grigori/Dropbox", title='Please select a zip file')
+    initialdir="/", title='Please select a zip file')
 targ_dir = askdirectory(
-    initialdir="/home/grigori/Dropbox", title='Please select a directory')
+    title='Please select a directory')
 
-print(targ_dir)
 
-dir_name = targ_dir + os.sep + zip_file.split(os.sep)[-1].split("_")[0]
+dir_name = targ_dir + "/" + zip_file.split("/")[-1].split("_")[0]
 print(dir_name)
 
 
@@ -23,8 +22,7 @@ os.mkdir(dir_name)
 fh = open(zip_file, 'rb')
 z = zipfile.ZipFile(fh)
 for name in z.namelist():
-    print(name)
-    doc_name = name.split(os.sep)[-1].split(" ")[1] + ".docx"
+    doc_name = name.split("/")[-1].split(" ")[1] + ".docx"
     source = z.open(name)
     target = file(os.path.join(dir_name, doc_name), "wb")
     with source, target:
