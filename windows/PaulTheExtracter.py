b4b6595312f47faee34c9944a73f1e6464f3844d
unknown <marcin.kopacz@AuroreCarric-PC.caliberi.local>
2014-12-17 11:18:38 +0000

    path se[arator

--- windows/PaulTheExtracter.py
+++ windows/PaulTheExtracter.py
@@ -12,7 +12,7 @@ zip_file = askopenfilename(
 targ_dir = askdirectory(
     initialdir="/", title='Please select a directory')
 
-dir_name = targ_dir + "/" + zip_file.split("/")[-1].split("_")[0]
+dir_name = targ_dir + os.pathsep + zip_file.split(os.pathsep)[-1].split("_")[0]
 
 os.mkdir(dir_name)
 
@@ -20,7 +20,7 @@ os.mkdir(dir_name)
 fh = open(zip_file, 'rb')
 z = zipfile.ZipFile(fh)
 for name in z.namelist():
-    doc_name = name.split("/")[-1].split(" ")[1] + ".docx"
+    doc_name = name.split(os.pathsep)[0].split(" ")[1] + ".docx"
     source = z.open(name)
     target = file(os.path.join(dir_name, doc_name), "wb")
     with source, target:
