93dccc73b9b6d3d678d07d887603521ed01e4ce2
Chyzwar <schizek.marcin@gmail.com>
2014-12-17 10:42:15 +0000

    version 1.0

---
+++ windows/PaulTheExtracter.py
@@ -0,0 +1,29 @@
+from Tkinter import Tk
+from tkFileDialog import askopenfilename, askdirectory
+import zipfile
+import os
+import shutil
+
+
+Tk().withdraw()
+
+zip_file = askopenfilename(
+    initialdir="/home/grigori/Dropbox", title='Please select a zip file')
+targ_dir = askdirectory(
+    initialdir="/home/grigori/Dropbox", title='Please select a directory')
+
+dir_name = targ_dir + "/" + zip_file.split("/")[-1].split("_")[0]
+
+os.mkdir(dir_name)
+
+
+fh = open(zip_file, 'rb')
+z = zipfile.ZipFile(fh)
+for name in z.namelist():
+    doc_name = name.split("/")[-1].split(" ")[1] + ".docx"
+    source = z.open(name)
+    target = file(os.path.join(dir_name, doc_name), "wb")
+    with source, target:
+        shutil.copyfileobj(source, target)
+
+fh.close()
